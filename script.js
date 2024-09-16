e3af5bf6400fef1f20a8e984e9cfcc5fddf54591
Adriana <150440061+Adrianaa911@users.noreply.github.com>
2024-09-16 20:52:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+// The switch keyword
+
+/*
+let groceryItem = 'papaya';
+
+if (groceryItem === 'tomato') {
+  console.log('Tomatoes are $0.49');
+} else if (groceryItem === 'papaya'){
+  console.log('Papayas are $1.29');
+} else {
+  console.log('Invalid item');
+}
+
+A switch statement provides an alternative syntax that is easier to read and write. A switch statement looks like this:
+
+let groceryItem = 'papaya';
+
+switch (groceryItem) {
+  case 'tomato':
+    console.log('Tomatoes are $0.49');
+    break;
+  case 'lime':
+    console.log('Limes are $1.49');
+    break;
+  case 'papaya':
+    console.log('Papayas are $1.29');
+    break;
+  default:
+    console.log('Invalid item');
+    break;
+}
+
+// Prints 'Papayas are $1.29'
+*/
+
+let athleteFinalPosition = 'first place';
+switch (athleteFinalPosition) {
+  case 'first place':
+  console.log('You get the gold medal!');
+  break;
+  case 'second place':
+  console.log('You get the silver medal!');
+  break;
+  case 'third place' :
+  console.log('You get the bronze medal!');
+  break;
+  default: 
+  console.log('No medal awarded.');
+  break;
+}

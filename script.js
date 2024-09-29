9dd2bec158933eee32e45389c1ee643d1e984751
Adriana <150440061+Adrianaa911@users.noreply.github.com>
2024-09-29 20:43:55 +0100

    Arrays methods added

--- script.js
+++ script.js
@@ -50,6 +50,8 @@ switch (athleteFinalPosition) {
 }
 
 
+
+// -- Arrays -- 
 /*
 Each element in an array has a numbered position known as its index. We can access individual items using their index, which is similar to referencing an item in a list based on the item’s position.
 
@@ -60,18 +62,44 @@ Arrays in JavaScript are zero-indexed, meaning the positions start counting from
 const hello = 'Hello World';
 console.log(hello[6]);
 
+// -- The method .push() --> allows us to add items to the end of an array. Here is an example of how this is used:
 
-/*
-Let’s learn about some built-in JavaScript methods that make working with arrays easier. These methods are specifically called on arrays to make common tasks, like adding and removing elements, more straightforward.
-
-One method, .push(), allows us to add items to the end of an array. Here is an example of how this is used:
-*/
 
 const itemTracker = ['item 0', 'item 1', 'item 2'];
 
 itemTracker.push('item 3', 'item 4');
 
 console.log(itemTracker); 
+// Output: ['item 0', 'item 1', 'item 2', 'item 3', 'item 4'];
+
+
+const chores = ['wash dishes', 'do laundry', 'take out trash'];
+
+chores.push('Wipe out the dust', 'Vaccum the house');
+console.log(chores);
+//Outout: ['wash dishes', 'do laundry', 'take out trash', 'Wipe out the dust', 'Vaccum the house'];
+
+
+// -- The .pop() Method
+
+//Another array method, .pop(), removes the last item of an array.
+
+const newItemTracker = ['item 0', 'item 1', 'item 2'];
+
+const removed = newItemTracker.pop();
+
+console.log(newItemTracker); 
+// Output: [ 'item 0', 'item 1' ]
+console.log(removed);
+// Output: item 2
+
+/*
+In the example above, calling .pop() on the newItemTracker array removed item 2 from the end.
+.pop() does not take any arguments, it simply removes the last element of newItemTracker.
+.pop() returns the value of the last element. In the example, we store the returned value in a variable removed to be used for later.
+.pop() is a method that mutates the initial array.
+When you need to mutate an array by removing the last element, use .pop().
+*/
 // Output: ['item 0', 'item 1', 'item 2', 'item 3', 'item 4'];
 
 

3a556708f1cbb899b1915d8c7dc81b751eec5bb2
Adriana <150440061+Adrianaa911@users.noreply.github.com>
2024-09-29 20:35:55 +0100

    Added Arrays

--- script.js
+++ script.js
@@ -48,3 +48,35 @@ switch (athleteFinalPosition) {
   console.log('No medal awarded.');
   break;
 }
+
+
+/*
+Each element in an array has a numbered position known as its index. We can access individual items using their index, which is similar to referencing an item in a list based on the item’s position.
+
+Arrays in JavaScript are zero-indexed, meaning the positions start counting from 0 rather than 1. Therefore, the first item in an array will be at position 0.
+
+*/
+
+const hello = 'Hello World';
+console.log(hello[6]);
+
+
+/*
+Let’s learn about some built-in JavaScript methods that make working with arrays easier. These methods are specifically called on arrays to make common tasks, like adding and removing elements, more straightforward.
+
+One method, .push(), allows us to add items to the end of an array. Here is an example of how this is used:
+*/
+
+const itemTracker = ['item 0', 'item 1', 'item 2'];
+
+itemTracker.push('item 3', 'item 4');
+
+console.log(itemTracker); 
+// Output: ['item 0', 'item 1', 'item 2', 'item 3', 'item 4'];
+
+
+const chores = ['wash dishes', 'do laundry', 'take out trash'];
+
+chores.push('Wipe out the dust', 'Vaccum the house');
+console.log(chores);
+//Outout: ['wash dishes', 'do laundry', 'take out trash', 'Wipe out the dust', 'Vaccum the house'];
